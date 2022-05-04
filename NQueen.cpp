78256ef6f16d44069b8df0d7d817d2212324832c
SUBHADEEP PATHAK <75271901+subhodynamics@users.noreply.github.com>
2022-05-04 20:59:36 +0530

    nQueen

---
+++ NQueen.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+std::vector<std::vector<string> > l(i, std::vector<string>(j, 0));
+
+int main(){
+
+    // write your code here
+
+
+    return 0;
+}
